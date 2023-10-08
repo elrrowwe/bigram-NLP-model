9ee3d61962087f6bd0806edff22aabb1ce6a4a53
elrrowwe <248607@edu.p.lodz.pl>
2023-10-08 14:14:05 +0200

    a working version of the bigram model

--- model.py
+++ model.py
@@ -9,11 +9,9 @@ This file contains my implementation of the bigram language model, as described 
 #opening the data file, taken from Andrej's github repo 
 words = open('names.txt', 'r').read().splitlines()
 
-print(f'the first 10 words from the list: \n {words[:10]}')
-
 counts_dict = {} #a dictionary to store
 for w in words:
-    chars = ['<S>'] + list(w) + ['<E>'] #adding special start and end tokens to each word
+    chars = ['<S>'] + list(w) + ['<E>'] #adding special start and end tokens to each word. they later on will be substitutet for '.'
     for i,j in zip(chars, chars[1:]):
         bigram = (i,j)
         counts_dict[bigram] = counts_dict.get(bigram, 0) + 1 #basically adding 1 to the value of a at bigram each time that bigram is encountered 
@@ -25,20 +23,17 @@ counts_tensor = torch.zeros((28,28), dtype=torch.int32)
 
 chars = sorted(list(set(''.join(words)))) #a string, containing the whole dataset (all the names, joined)
 
-#defining a simple string to integer tokenizer 
-stoi = {s:i for i,s in enumerate(chars)}
+#defining a simple string to integer tokenizer (a mapping from strings to integers) 
+stoi = {s:i+1 for i,s in enumerate(chars)}
 
-#adding tokens for the special characters, since they are not included in the dataset (a mapping from strings to integers)
-stoi['<S>'] = 26 
-stoi['<E>'] = 27
+#adding a token for a special character, since it is not included in the dataset 
+stoi['.'] = 0
 
 #now, defining a mapping from integers to strings (essentially reverses the stoi tokenizer)
-itos = {i:s for i,s in enumerate(chars)}
-itos[26] = '<S>'
-itos[27] = '<E>'
+itos = {i:s for s,i in stoi.items()}
 
 for w in words:
-    chars = ['<S>'] + list(w) + ['<E>'] #adding special start and end tokens to each word
+    chars = ['.'] + list(w) + ['.'] #adding special start and end tokens to each word
     for i,j in zip(chars, chars[1:]):
         ind_i = stoi[i]
         ind_j = stoi[j]
@@ -55,3 +50,15 @@ for w in words:
 #         plt.text(j, i, counts_tensor[i, j].item(), ha="center", va="top", color='gray')
 # plt.axis('off')
 # plt.show()
+
+#the sampling loop
+ind = 0
+output = []
+while True:
+    p = counts_tensor[ind].float() #the tensor (row of counts_tensor), corresponding to the current letter 
+    p /= p.sum()
+    ind = torch.multinomial(p, num_samples=1, replacement=True).item() #sampling a character from the probability distribution tensor
+    output.append(itos[ind])
+    if ind == 0: #if the index sampled is the end token -- break the loop
+        break
+print(''.join(output))
